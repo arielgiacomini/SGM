25cab89dc980ef436511ab32967b62e10f3aebf0
Ariel Giacomini <ariel.giacomini@minutoseguros.com.br>
2020-11-23 08:36:08 -0300

    Limpeza da BLLPeca

--- src/BLL/BLLPeca.cs
+++ src/BLL/BLLPeca.cs
@@ -1,5 +1,4 @@
 ﻿using DAL;
-using Modelo;
 using System;
 using System.Data;
 
@@ -14,59 +13,10 @@ namespace BLL
             this.conexao = cx;
         }
 
-        public void Incluir(ModeloPeca modelo)
-        {
-            if (modelo.CDescricao.Trim().Length == 0)
-            {
-                throw new Exception("O nome da peça é obrigatório.");
-            }
-
-            if (modelo.CValor == 0)
-            {
-                throw new Exception("O valor da peça é obrigatório.");
-            }
-
-            modelo.CDescricao = modelo.CDescricao.ToUpper();
-            modelo.CFornecedor = modelo.CFornecedor.ToUpper();
-
-            DALPeca DALobj = new DALPeca(conexao);
-            DALobj.Incluir(modelo);
-        }
-
-        public void Alterar(ModeloPeca modelo)
-        {
-            if (modelo.CPecaId <= 0)
-            {
-                throw new Exception("O código da Peça é obrigatório!");
-            }
-            if (modelo.CDescricao.Trim().Length == 0)
-            {
-                throw new Exception("O nome da Peça é obrigatório.");
-            }
-
-            modelo.CDescricao = modelo.CDescricao.ToUpper();
-            modelo.CFornecedor = modelo.CFornecedor.ToUpper();
-
-            DALPeca DALobj = new DALPeca(conexao);
-            DALobj.Alterar(modelo);
-        }
-
-        public void Excluir(int pecaid)
-        {
-            DALPeca DALobj = new DALPeca(conexao);
-            DALobj.Excluir(pecaid);
-        }
-
         public DataTable Localizar(String valor)
         {
             DALPeca DALobj = new DALPeca(conexao);
             return DALobj.Localizar(valor);
-        }
-
-        public ModeloPeca CarregaModeloPeca(int pecaid)
-        {
-            DALPeca DALobj = new DALPeca(conexao);
-            return DALobj.CarregaModeloPeca(pecaid);
         }
     }
 }
