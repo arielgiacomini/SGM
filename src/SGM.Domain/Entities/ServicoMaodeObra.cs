e00eadc92ef278510e04eb82a9cf73c952be311b
Ariel Giacomini <arieltecnologia@outlook.com>
2021-08-07 17:08:01 -0300

    Retirando o lixo

--- src/SGM.Domain/Entities/ServicoMaodeObra.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SGM.Domain.Entities
-{
-    public class ServicoMaodeObra
-    {
-        public int Id { get; set; }
-        public int ServicoId { get; set; }
-        public int MaodeObraId { get; set; }
-    }
-}
--- src/SGM.Domain/Entities/ServicoPeca.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SGM.Domain.Entities
-{
-    public class ServicoPeca
-    {
-        public int Id { get; set; }
-        public int ServicoId { get; set; }
-        public int PecaId { get; set; }
-    }
-}
